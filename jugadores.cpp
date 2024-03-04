dee1434a914ff15e94e927802cbe7add85199d77
gabriel250206 <19050@portalcedi.edu.mx>
2024-03-04 14:51:02 -0600

    terminado

--- jugadores.cpp
+++ jugadores.cpp
@@ -11,21 +11,47 @@ class Jugador{
         Agrega propiedades para guardar la vida(número entero), ataque(número entero) y nombre del jugador.
         Agrega una propiedad para guardar el jugador que lo atacó por última vez.
     */
-    
-    Jugador(); 
-    Jugador(/* agrega parámetros para asignar hp, ataque y nombre */); 
-    
-    void atacar(/* agregar parámetros para atacar a otro jugador*/); 
+    int hp;
+    int ataque;
+    string nombre;
+    string uAtacante;
+    public:
+    Jugador();
+    Jugador(int hp, int ataque, string nombre/* agrega parámetros para asignar hp, ataque y nombre */);
+
+    void atacar(Jugador &objetivo/* agregar parámetros para atacar a otro jugador*/);
+    int getHp(){
+        return this->hp;
+    }
+    string getName(){
+        return this->nombre;
+    }
 };
 
 int main()
 {
     srand(time(0));  // Seed para generar valores aleatorios
-    
+    Jugador abraham{100,5,"Abraham"};
+    Jugador eleonor{200,20,"Eleonor"};
+    while(eleonor.getHp()>0&&abraham.getHp()>0){
+        if(eleonor.getHp()>0){
+            eleonor.atacar(abraham);
+        }
+        if(abraham.getHp()>0){
+            abraham.atacar(eleonor);
+        }
+        ;
+    }
+    if(eleonor.getHp()>abraham.getHp()){
+        cout<<eleonor.getName()<<" fue la vencedora"<<endl;
+    }else{
+        cout<<abraham.getName()<<" fue el vencedor"<<endl;
+    }
+
     /*
-        Crear al menos 2 jugadores con diferentes nombres, vida y ataque. 
+        Crear al menos 2 jugadores con diferentes nombres, vida y ataque.
         Los jugadores deben atacarse entre sí, tomando turnos, hasta que uno pierda toda su vida.
-        En cuanto la vida del jugador se reduzca a 0 el programa muestra al ganador y se termina. 
+        En cuanto la vida del jugador se reduzca a 0 el programa muestra al ganador y se termina.
         No importa quien ataca primero.
     */
 
@@ -33,13 +59,38 @@ int main()
 }
 
 Jugador::Jugador(){
+    this->hp=100;
+    this->ataque=5;
+    this->nombre="no definido";
+
     /*
         Asignar 100 a hp y 5 a attack como valores default.
         Asignar 'No definido' al nombre como default.
     */
 }
 
-Jugador::Jugador(/* Agrega parámetros para asignar hp, ataque y nombre*/){
+Jugador::Jugador(int hp, int ataque, string nombre/* Agrega parámetros para asignar hp, ataque y nombre*/){
+    if(hp>0){
+        if(hp>=200){
+            this->hp=200;
+        }else{
+            this->hp=hp;
+        }
+    }else{
+        this->hp=1;
+    }
+
+if(ataque>0){
+    if(ataque>=20){
+        this->ataque=20;
+    }else{
+        this->ataque=ataque;
+    }
+}else{
+        this->ataque=1;
+}
+
+    this->nombre=nombre;
     /*
         Asignar los parámetros recibidos a las propiedades del objeto.
         No se pueden asignar valores negativos a hp y attack.
@@ -47,7 +98,18 @@ Jugador::Jugador(/* Agrega parámetros para asignar hp, ataque y nombre*/){
     */
 }
 
-void Jugador::atacar(/* Agregar parámetros para atacar a otro Jugador */){
+void Jugador::atacar(Jugador &objetivo/* Agregar parámetros para atacar a otro Jugador */){
+
+    int x=rand()%5;
+    if(x!=0){
+        objetivo.hp=objetivo.hp-this->ataque;
+        cout<<this->nombre<<" ataco a "<<objetivo.nombre<<" he hizo "<<this->ataque<<" de danio, la vida restante de "<<objetivo.nombre<<"= "<<objetivo.hp<<endl;
+        objetivo.uAtacante=this->nombre;
+    }else{
+        cout<<this->nombre<<" ataco a "<<objetivo.nombre<<" pero fallo, la vida restante de "<<objetivo.nombre<<"= "<<objetivo.hp<<endl;
+        objetivo.uAtacante=this->nombre;
+    }
+
     /*
         El jugador atacado(el que recibes como parámetro) pierde HP igual al ataque del jugador que ataca.
         Existe un 20% de probabilidad de que el ataque falle y no haga daño.
